054ad75bfc5d46ceb73a14881a7fbca1204a3118
mwjung <mwjung10@gmail.com>
2025-04-12 12:59:47 +0200

    Addem minimax function

--- p_minmax.py
+++ p_minmax.py
@@ -15,8 +15,28 @@ class MinMaxPlayer(Player):
         # TODO
         return None
 
-    def minimax(self, board: Board, side: str, depth: int):
-        #TO DO
+    def minimax(self, board: Board, side: str, depth: int, alpha=float('-inf'), beta=float('inf')):
+        if board.who_is_winner() or not board.empty_indexes() or depth == 0:
+            return self.evaluate(board, side)
+
+        moves = board.empty_indexes()
+
+        if side == board.player_chars[0]:  # Circle's turn
+            for move in moves:
+                new_board = board.clone()
+                new_board.register_move(move)
+                alpha = max(alpha, self.minimax(new_board, board.player_chars[1], depth - 1, alpha, beta))
+                if alpha >= beta:
+                    return beta
+            return alpha
+        else:  # Cross's turn
+            for move in moves:
+                new_board = board.clone()
+                new_board.register_move(move)
+                beta = min(beta, self.minimax(new_board, board.player_chars[0], depth - 1, alpha, beta))
+                if beta <= alpha:
+                    return alpha
+            return beta
 
     def evaluate(self, board: Board, side: str):
         # TODO
